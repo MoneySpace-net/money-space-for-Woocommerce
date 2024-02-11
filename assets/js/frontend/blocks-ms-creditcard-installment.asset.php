e60c6d2e3446dc377c073ab2cd40cf81b667fee7
Ekapop.B <bus.ekapop@gmail.com>
2024-02-11 14:55:39 +0700

    implement cc installment form for blocked-based

--- assets/js/frontend/blocks-ms-creditcard-installment.asset.php
+++ assets/js/frontend/blocks-ms-creditcard-installment.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'a5f314469a0831208a22');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '8fe02ac0e49917102c9f');
--- includes/assets/js/components/CreditCardInstallmentForm.js
+++ includes/assets/js/components/CreditCardInstallmentForm.js
@@ -4,16 +4,23 @@ import { __ } from '@wordpress/i18n';
 
 const CreditCardInstallmentForm = (props) => {
     console.log('props', props);
-    const paymentData = {
+    const model = {
         selectbank: "",
         KTC_permonths: "",
         BAY_permonths: "",
-        FCY_permonths: ""
+        FCY_permonths: "",
+        dirty: false
     };
+    const [paymentData, setPaymentData] = useState(model);
     const { ccIns } = props;
     const { cartTotal, currency } = props.billing;
     const { onPaymentSetup, onPaymentProcessing, onCheckoutValidationBeforeProcessing } = props.eventRegistration;
     console.log('ccInsIcons', ccIns, ccIns.filter(x => x.code == "ktc")[0].icon);
+
+    const handleChange = (field) => (event) => {
+        setFormData({ ...formData, [field]: event.target.value, ["dirty"]: true });
+    };
+
     const checkPrice = () => {
         cartTotal.value
         var total = cartTotal.value / Math.pow(10, currency.minorUnit);
@@ -50,32 +57,35 @@ const CreditCardInstallmentForm = (props) => {
             <h2>เลือกการผ่อนชำระ</h2>
             <div className={`wc-block-components-radio-control`}>
                 <div class="wc-block-components-radio-control-accordion-option">
-                    <label class="wc-block-components-radio-control__option" for="radio-control-wc-payment-method-options-moneyspace">
-                        <input id="radio-control-wc-payment-method-options-moneyspace" class="wc-block-components-radio-control__input" type="radio" name="radio-control-wc-payment-method-options" aria-describedby="radio-control-wc-payment-method-options-moneyspace__label" value="moneyspace" checked="" />
+                    <label class="wc-block-components-radio-control__option" for="radio-control-wc-payment-method-options-moneyspace-ins-ktc">
+                        <input id="radio-control-wc-payment-method-options-moneyspace-ins-ktc" class="wc-block-components-radio-control__input" type="radio" name="radio-control-wc-payment-method-options-ins-ktc" aria-describedby="radio-control-wc-payment-method-options-moneyspace__label" value="moneyspace-ins-ktc"  checked="true" />
                         <div class="wc-block-components-radio-control__option-layout">
                             <div class="wc-block-components-radio-control__label-group">
                                 <span id="radio-control-wc-payment-method-options-moneyspace__label" class="wc-block-components-radio-control__label">
-                                    <div class="wc-moneyspace-blocks-payment-method__label moneyspace">
+                                    <div class="wc-moneyspace-blocks-payment-method__label moneyspace-ins-ktc">
                                         <span class="wc-block-components-payment-method-label">{ccIns.filter(x => x.code == "ktc")[0].label}</span>
                                         <div class="wc-block-components-payment-method-icons">
-                                            <img class="wc-block-components-payment-method-icon wc-block-components-payment-method-icon--moneyspace" src={ccIns.filter(x => x.code == "ktc")[0].icon} alt="moneyspace" />
+                                            <img class="wc-block-components-payment-method-icon wc-block-components-payment-method-icon--moneyspace" src={ccIns.filter(x => x.code == "ktc")[0].icon} alt="moneyspace-ins-ktc" />
                                         </div>
                                     </div>
                                 </span>
                             </div>
                         </div>
                     </label>
+                    <div className='wc-block-components-radio-control-accordion-content'>
+                        <h1>test</h1>
+                    </div>
                 </div>
                 <div class="wc-block-components-radio-control-accordion-option">
-                    <label class="wc-block-components-radio-control__option" for="radio-control-wc-payment-method-options-moneyspace">
-                        <input id="radio-control-wc-payment-method-options-moneyspace" class="wc-block-components-radio-control__input" type="radio" name="radio-control-wc-payment-method-options" aria-describedby="radio-control-wc-payment-method-options-moneyspace__label" value="moneyspace" checked="" />
+                    <label class="wc-block-components-radio-control__option" for="radio-control-wc-payment-method-options-moneyspace-ins-bay">
+                        <input id="radio-control-wc-payment-method-options-moneyspace-ins-bay" class="wc-block-components-radio-control__input" type="radio" name="radio-control-wc-payment-method-options-ins-bay" aria-describedby="radio-control-wc-payment-method-options-moneyspace__label" value="moneyspace-ins-bay" checked="" />
                         <div class="wc-block-components-radio-control__option-layout">
                             <div class="wc-block-components-radio-control__label-group">
                                 <span id="radio-control-wc-payment-method-options-moneyspace__label" class="wc-block-components-radio-control__label">
-                                    <div class="wc-moneyspace-blocks-payment-method__label moneyspace">
+                                    <div class="wc-moneyspace-blocks-payment-method__label moneyspace-ins-bay">
                                         <span class="wc-block-components-payment-method-label">{ccIns.filter(x => x.code == "bay")[0].label}</span>
                                         <div class="wc-block-components-payment-method-icons">
-                                            <img class="wc-block-components-payment-method-icon wc-block-components-payment-method-icon--moneyspace" src={ccIns.filter(x => x.code == "bay")[0].icon} alt="moneyspace" />
+                                            <img class="wc-block-components-payment-method-icon wc-block-components-payment-method-icon--moneyspace" src={ccIns.filter(x => x.code == "bay")[0].icon} alt="moneyspace-ins-bay" />
                                         </div>
                                     </div>
                                 </span>
@@ -84,15 +94,15 @@ const CreditCardInstallmentForm = (props) => {
                     </label>
                 </div>
                 <div class="wc-block-components-radio-control-accordion-option">
-                    <label class="wc-block-components-radio-control__option" for="radio-control-wc-payment-method-options-moneyspace">
-                        <input id="radio-control-wc-payment-method-options-moneyspace" class="wc-block-components-radio-control__input" type="radio" name="radio-control-wc-payment-method-options" aria-describedby="radio-control-wc-payment-method-options-moneyspace__label" value="moneyspace" checked="" />
+                    <label class="wc-block-components-radio-control__option" for="radio-control-wc-payment-method-options-moneyspace-ins-fcy">
+                        <input id="radio-control-wc-payment-method-options-moneyspace-ins-fcy" class="wc-block-components-radio-control__input" type="radio" name="radio-control-wc-payment-method-options-ins-fcy" aria-describedby="radio-control-wc-payment-method-options-moneyspace__label" value="moneyspace-ins-fcy" checked="" />
                         <div class="wc-block-components-radio-control__option-layout">
                             <div class="wc-block-components-radio-control__label-group">
                                 <span id="radio-control-wc-payment-method-options-moneyspace__label" class="wc-block-components-radio-control__label">
-                                    <div class="wc-moneyspace-blocks-payment-method__label moneyspace">
+                                    <div class="wc-moneyspace-blocks-payment-method__label moneyspace-ins-fcy">
                                         <span class="wc-block-components-payment-method-label">{ccIns.filter(x => x.code == "fcy")[0].label}</span>
                                         <div class="wc-block-components-payment-method-icons">
-                                            <img class="wc-block-components-payment-method-icon wc-block-components-payment-method-icon--moneyspace" src={ccIns.filter(x => x.code == "fcy")[0].icon} alt="moneyspace" />
+                                            <img class="wc-block-components-payment-method-icon wc-block-components-payment-method-icon--moneyspace" src={ccIns.filter(x => x.code == "fcy")[0].icon} alt="moneyspace-ins-fcy" />
                                         </div>
                                     </div>
                                 </span>
