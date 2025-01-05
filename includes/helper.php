489a73ba35a6f4c4daea4e8fbbae8e35b2ee2c97
Ekapop.B <bus.ekapop@gmail.com>
2025-01-05 20:58:11 +0700

    2.13.1 : fix bug update order status after pay

--- includes/helper.php
+++ includes/helper.php
@@ -111,7 +111,6 @@ function update_order_status($order) {
         $payment_gateway_qr = $payment_gateways->payment_gateways()[$payment_gateway_qr_id];
         $payment_gateway_installment = $payment_gateways->payment_gateways()[$payment_gateway_installment_id];
     
-        // $gateways = WC()->payment_gateways->get_available_payment_gateways();
         $ms_secret_id = $payment_gateway->settings['secret_id'];
         $ms_secret_key = $payment_gateway->settings['secret_key'];
         $ms_stock_setting = $payment_gateway->settings['ms_stock_setting'];
@@ -119,13 +118,9 @@ function update_order_status($order) {
         $ms_order_select = $payment_gateway->settings['order_status_if_success'];
         $ms_order_select_qr = $payment_gateway_qr->settings['order_status_if_success'];
         $ms_order_select_installment = $payment_gateway_installment->settings['order_status_if_success'];
-    
-        $enable_auto_check_result = $payment_gateway_qr->settings['enable_auto_check_result'];
-    
-        // $ms_time = date("YmdHis");
+
         $MNS_transaction_orderid = get_post_meta($order->id, 'MNS_transaction_orderid', true);
         $MNS_PAYMENT_TYPE = get_post_meta($order->id, 'MNS_PAYMENT_TYPE', true);
-        // $order_amount = $order->get_total();
         $check_orderid = wp_remote_post(MNS_API_URL_CHECK, array(
             'method' => 'POST',
             'timeout' => 120,
@@ -158,10 +153,6 @@ function update_order_status($order) {
                         $order->update_status("wc-processing");
                     }else{
                         $order->update_status($ms_order_select_qr);
-    
-                        if($enable_auto_check_result == "yes" || $enable_auto_check_result == "") {
-                            // wp_redirect(wc_get_order($order->id)->get_checkout_order_received_url());
-                        }
                     }
                 } else if($MNS_PAYMENT_TYPE == "Installment"){
     
@@ -177,22 +168,14 @@ function update_order_status($order) {
                 }
                 update_post_meta($order->id, 'MNS_PAYMENT_PAID', $ms_status->amount);
                 update_post_meta($order->id, 'MNS_PAYMENT_STATUS', $ms_status->status);
-                // wp_redirect(wc_get_order($order->id)->get_checkout_order_received_url());
             } else if ($ms_status->status == "Cancel") {
                 $order->update_status("wc-cancelled");
-                // wp_redirect(wc_get_order($order->id)->get_cancel_order_url());
-            } else {
-                # Fail case
-                $order->update_status("wc-failed");
-                // wp_redirect(wc_get_order($order->id)->get_checkout_order_received_url());
             }
         } else {
-            $order->update_status("wc-failed");
-            // wp_redirect(wc_get_order($order->id)->get_checkout_order_received_url());
+            
         }
-        WC()->cart->empty_cart();
     } else {
-        // wp_redirect(wc_get_order($order->id)->get_checkout_order_received_url());
+
     }
 }
 
