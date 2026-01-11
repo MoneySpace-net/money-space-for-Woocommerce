758b5d98fc07ff7e0580bfc40b8199e19ca4c6da
Ekapop.B <bus.ekapop@gmail.com>
2026-01-12 01:39:56 +0700

    fix installment

--- assets/js/frontend/blocks-ms-creditcard-installment.asset.php
+++ assets/js/frontend/blocks-ms-creditcard-installment.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'd57add3e7cad58dfad67');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '19bfcd59d597eb018df0');
